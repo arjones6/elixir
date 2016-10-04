096193554f728df04fc0c93f959929eda27f5a31
Ivo Tzvetkov <ivotkv@gmail.com>
2016-10-04 09:41:44 -0400

    incrementing to v0.7.3, all tests pass

--- elixir/__init__.py
+++ elixir/__init__.py
@@ -38,7 +38,7 @@ from elixir.statements import Statement
 from elixir.collection import EntityCollection, GlobalEntityCollection
 
 
-__version__ = '0.7.2'
+__version__ = '0.7.3'
 
 __all__ = ['Entity', 'EntityBase', 'EntityMeta', 'EntityCollection',
            'entities',
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name="elixir",
-      version="0.7.2",
+      version="0.7.3",
       description="Declarative Mapper for SQLAlchemy",
       long_description="""
 Elixir
